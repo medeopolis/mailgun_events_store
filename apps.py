861374f152dfb646ef7aa36263a596559bd67437
root <root@mcp>
2019-02-08 21:09:45 +1100

    Initial commit of plain django app
    Created by manage.py startapp, this will be for storing code which is
    used for storing Mailgun events ("logs") for future use.

---
+++ apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class MailgunEventsStoreConfig(AppConfig):
+    name = 'mailgun_events_store'
