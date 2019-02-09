3deaa05dad95b3bc7ab2bebe9e9bb57b66d7798d
root <root@mcp>
2019-02-09 21:30:20 +1100

    Add model for storing events
    This model has four values:
    the three values which mailgun promise to have in every event (id,
    timestamp, event (type), plus json which holds the entire event entry.

--- models.py
+++ models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class MailgunEvent(models.Model):
+    event_type = models.CharField(max_length=30)
+    event_id = models.CharField(max_length=254)
+    timestamp = models.CharField(max_length=30)
+    json = models.TextField(null=True, blank=True)
+
