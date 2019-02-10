4018ea0bf6759a709309ee4af7e14442aa8c8989
root <root@mcp>
2019-02-10 21:28:16 +1100

    Augment Mailgun Event admin page
    Add more columns list display and add a filter for event type.

--- admin.py
+++ admin.py
@@ -3,8 +3,18 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from datetime import datetime
+
 # Register your models here.
 from .models import MailgunEvent
 
-admin.site.register(MailgunEvent)
+class MailgunEventAdmin(admin.ModelAdmin):
+    list_display = ('event_type', 'display_timestamp', 'event_id')
+    list_filter = ('event_type',)
 
+    def display_timestamp(self, obj):
+        return datetime.fromtimestamp(float(obj.timestamp))
+
+admin.site.register(MailgunEvent, MailgunEventAdmin)
+
+# add display options
