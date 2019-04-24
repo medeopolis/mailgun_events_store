cb2c526a060743d5845b26979155bd9c762f346a
Karl Goetz <karl@kgoetz.id.au>
2019-04-24 21:16:13 +1000

    Add support for separated data in MailgunEvent
    To make integration (via email address) and graphing/processing of data
    easier I'm adding new fields to MailgunEvent for every item I know will
    be recieved from Mailgun. Others may need to be added in future - one
    reason to keep the full json around!
    
    This starts to address issue #1.

--- models.py
+++ models.py
@@ -9,7 +9,26 @@ class MailgunEvent(models.Model):
     event_type = models.CharField(max_length=30)
     event_id = models.CharField(max_length=254)
     timestamp = models.CharField(max_length=30)
-    json = models.TextField(null=True, blank=True)
+    json = models.TextField(default='', blank=True)
+    # New, not guaranteed available fields
+    log_level = models.CharField(default='', blank=True, max_length=30)
+    method = models.CharField(default='', blank=True, max_length=30)
+    envelope = models.TextField(default='', blank=True)
+    flags = models.TextField(default='', blank=True)
+    delivery_status = models.TextField(default='', blank=True)
+    geolocation = models.TextField(default='', blank=True)
+    message = models.TextField(default='', blank=True)
+    severity = models.CharField(default='', blank=True, max_length=30)
+    storage = models.TextField(default='', blank=True)
+    reason = models.CharField(default='', blank=True, max_length=255)
+    recipient = models.CharField(default='', blank=True, max_length=255)
+    recipient_domain = models.CharField(default='', blank=True, max_length=255)
+    campaigns = models.TextField(default='', blank=True)
+    tags = models.TextField(default='', blank=True)
+    user_variables = models.TextField(default='', blank=True)
+    routes = models.TextField(default='', blank=True)
+    ip = models.CharField(default='', blank=True, max_length=30)
+    client_info = models.TextField(default='', blank=True)
 
     def __unicode__(self):
         # TODO: output timestamp in a human readable format
